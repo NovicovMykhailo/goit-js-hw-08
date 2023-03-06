ab4435f016e39e8b20e69ab56c19c0390b6476bb
NovicovMykhailo <mikenovicov@gmail.com>
2023-03-06 20:59:15 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,11 @@
+import Player from '@vimeo/player';
+
+const videoFrameEl = document.querySelector('#vimeo-player');
+
+const player = new Player(videoFrameEl);
+
+player.on('timeupdate', function (e) {
+  console.log(e);
+});
+
 
